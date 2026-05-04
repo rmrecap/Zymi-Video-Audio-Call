cf27b62c098bc08d7d82adbd20cc09b83134b2de
rmrecap <rmrecap@gmail.com>
2026-05-04 22:06:40 +0400

    update

--- tools/run_on_device.ps1
+++ tools/run_on_device.ps1
@@ -1,9 +1,21 @@
-# Run Flutter on Device from D Drive
+# Set Environment Variables
+$env:JAVA_HOME = "C:\Program Files\Android\Android Studio\jbr"
 $env:GRADLE_USER_HOME = "D:\G"
 $env:TEMP = "D:\android-temp"
 $env:TMP = "D:\android-temp"
 
-Set-Location mobile\zymi_mobile_app
+# Explicitly go to the Android project folder
+$AndroidDir = "D:\QiBo\mobile\zymi_mobile_app\android"
+if (Test-Path $AndroidDir) {
+    Set-Location $AndroidDir
+    Write-Host "Current Location: $(Get-Location)"
+} else {
+    Write-Error "Could not find Android directory at $AndroidDir"
+    exit
+}
 
-Write-Host "Starting flutter run on device..."
-flutter run -d 48ZYD25511402050 --android-skip-build-dependency-validation
+# Go back to flutter project root to run flutter command
+Set-Location "D:\QiBo\mobile\zymi_mobile_app"
+
+Write-Host "Starting flutter run..."
+flutter run --android-skip-build-dependency-validation
